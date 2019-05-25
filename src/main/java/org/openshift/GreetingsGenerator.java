cbb0dcc11f4bcf830e6b6a4402302c9cf85c7044
woland7 <antonio.deiasio@gmail.com>
2019-05-25 15:17:08 +0200

    Added new feature for a demo

--- src/main/java/org/openshift/GreetingsGenerator.java
+++ src/main/java/org/openshift/GreetingsGenerator.java
@@ -20,7 +20,7 @@ public class GreetingsGenerator {
 			Connection connection = DriverManager.getConnection(databaseURL, username, password);
 
 			if (connection != null) {
-				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
+				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from saluti a , appellativi b, domande c ORDER BY random() limit 1";
 				Statement stmt = connection.createStatement();
 				ResultSet rs = stmt.executeQuery(SQL);
 				while (rs.next()) {

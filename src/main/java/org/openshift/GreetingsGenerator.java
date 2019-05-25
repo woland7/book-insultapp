5f1fc2731300ebeb050e57750aa4e19ece2d9894
woland7 <antonio.deiasio@gmail.com>
2019-05-25 17:25:50 +0200

    prova1

--- src/main/java/org/openshift/GreetingsGenerator.java
+++ src/main/java/org/openshift/GreetingsGenerator.java
@@ -6,7 +6,9 @@ import java.sql.Statement;
 
 public class GreetingsGenerator {
 	public String generateGreeting() {
-		String theGreeting = "";
+		String vowels = "AEIOU";
+		String article = "an";
+		String theInsult = "";
 
 		try {
 			String databaseURL = "jdbc:postgresql://";
@@ -18,12 +20,14 @@ public class GreetingsGenerator {
 			Connection connection = DriverManager.getConnection(databaseURL, username, password);
 
 			if (connection != null) {
-				System.out.println("connected");
-				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from saluti a , appellativi b, domande c ORDER BY random() limit 1";
+				String SQL = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
 				Statement stmt = connection.createStatement();
 				ResultSet rs = stmt.executeQuery(SQL);
 				while (rs.next()) {
-					theGreeting =  String.format("%s %s %s",rs.getString("first"),
+					if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
+						article = "a";
+					}
+					theInsult =  String.format("Thou art %s %s %s %s!", article, rs.getString("first"),
 							rs.getString("second"), rs.getString("noun"));
 				}
 				rs.close();
@@ -32,6 +36,6 @@ public class GreetingsGenerator {
 		} catch (Exception e) {
 			return "Database connection problem!";
 		}
-		return theGreeting;
+		return theInsult;
 	}
 }

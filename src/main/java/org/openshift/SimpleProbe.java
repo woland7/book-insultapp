9692f5e564e0809f5f55f61fcd76bc7b217621bf
woland7 <antonio.deiasio@gmail.com>
2019-05-26 17:33:59 +0200

    Added probe

---
+++ src/main/java/org/openshift/SimpleProbe.java
@@ -0,0 +1,27 @@
+package org.openshift;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/simpleprobe")
+public class SimpleProbe extends HttpServlet {
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SimpleProbe() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+}
